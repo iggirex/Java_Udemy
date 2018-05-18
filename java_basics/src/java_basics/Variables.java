937cd3fa5e039811d62a7fd08b7c43aaa5c2a0ab
unknown <iggirex@gmail.com>
2018-05-17 22:49:10 -0600

    adding beginnings of learning Java

---
+++ java_basics/src/java_basics/Variables.java
@@ -0,0 +1,20 @@
+package java_basics;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int x = 4;
+
+		System.out.println(x);
+		
+		x = 234;
+		
+		System.out.println(x + 4);
+		
+		String words = "20";
+		
+		System.out.println(words + " and some other words");
+	}
+
+}
---
+++ java_basics/src/java_basics/VariablesContinued.java
@@ -0,0 +1,15 @@
+package java_basics;
+
+public class VariablesContinued {
+
+	public static void main(String[] args) {
+
+		// Numbers can take up a lot of memory space
+		// so it's best to use the most appropriate data type
+		int var= 1000000000; // medium sized number holder
+		long bigNumber = 1000000000000000000L; // large number holder
+		short smallNumber = 32767; // small number holder
+
+	}
+
+}
