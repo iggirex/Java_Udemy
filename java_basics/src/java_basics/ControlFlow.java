245ffce1a9d1f4cf35bbe5b818578704177fe5ea
unknown <iggirex@gmail.com>
2018-05-18 21:12:16 -0600

    added control flow

--- java_basics/src/java_basics/ControlFlow.java
+++ java_basics/src/java_basics/ControlFlow.java
@@ -3,7 +3,36 @@ package java_basics;
 public class ControlFlow {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		boolean hungry = true;
+		
+		if(hungry) {
+			System.out.println("I'm starving");
+		} else {
+			System.out.println("I'm not hungry");
+		}
+		
+		
+		// Swich case statements better to read than ifs
+		int month = 4;
+		String monthString;
+		
+		switch(month) {
+		
+			case 1: monthString = "Jan";
+				break;
+			case 2: monthString = "Feb";
+				break;
+			case 3: monthString = "March";
+				break;
+			case 4: monthString = "April";
+				break;
+			default: monthString = "Unknown Month";
+				break;
+		}
+		
+		
+		System.out.println(monthString);
 
 	}
 
