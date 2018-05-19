6db463811cd0d8433b1da41ef75552b0c35ce1c0
unknown <iggirex@gmail.com>
2018-05-18 20:37:44 -0600

    super basic stuff

---
+++ java_basics/src/java_basics/LearningArrays.java
@@ -0,0 +1,32 @@
+package java_basics;
+
+public class LearningArrays {
+
+	public static void main(String[] args) {
+
+		int value;
+		value = 100;
+		
+		int [] values = new int[100];
+		
+		double [] decimalCollection = new double [5];
+		
+		values[0] = 1000;
+		
+		values[99] = 238904;
+		
+		decimalCollection[0] = 234.54356;
+		
+		String [] words = new String [] {"hey", "my", "dude"};
+		
+		System.out.println(words[2]);
+		
+		words = new String[10];
+		
+		System.out.println(words[9]);
+		
+		System.out.println(values[55]);
+
+	}
+
+}
