39490e02619ceef3fe792939e17a9891c21107a0
unknown <iggirex@gmail.com>
2018-05-20 14:35:47 -0600

    starting auto dealership

--- car_dealership/src/car_dealership/Dealership.java
+++ car_dealership/src/car_dealership/Dealership.java
@@ -5,15 +5,32 @@ public class Dealership {
 	public static void main(String[] args) {
 		// write a program to simulate car dealership -- employees, vehicles, customers
 		
-		Customer cust1 = new Customer("Tom", "123 Anything St.", 12000);
+//		Customer cust1 = new Customer("Tom", "123 Anything St.", 12000);
 		
+		Customer cust1 = new Customer();
+
+		cust1.setName("Tom");
+		cust1.setAddress("123 Anything St.");
+		cust1.setCashOnHand(12000);
+		
+		
+		// This is for public vars in a class
 //		cust1.name = "Tom";
 //		cust1.address = "123 Anything St.";
 //		cust1.cashOnHand = 12000;
 		
-		System.out.println(cust1.name);
+		System.out.println(cust1);
 		
-		cust1.purchaseCar(vehicle, emp, false);
+//		cust1.purchaseCar(vehicle, emp, false);
+		
+		
+		public static void generateDailyReport() {
+			System.out.println("daily report");
+		}
+		
+
+		
+
 
 	}
 
--- car_dealership/src/car_dealership/Employee.java
+++ car_dealership/src/car_dealership/Employee.java
@@ -1,5 +1,21 @@
 package car_dealership;
 
 public class Employee {
+	
+	
+	int talent;
+	String name;
 
+	
+	/**
+	 * handleCustomer( Customer cust, boolean finance, Vehicle vehicle)
+	 * if(finance == true)
+	 * 		runCreditHistory(Customer cust, double diffVehiclePriceCashOnHand)//in Customer
+	 * else if(vehicle.getPrice() <= cashOnHand);
+	 * 		processTransaction(Customer cust, Vehicle vehicle)
+	 * else
+	 * 		tell cust to bring more money
+	 * 
+	 * could add bonus money from offer
+	 */
 }
