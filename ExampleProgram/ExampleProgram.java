36ea3a3fd6a57aa0176667bcab4d0d9d2d9fae90
unknown <iggirex@gmail.com>
2018-05-23 21:46:59 -0600

    done with simple Jar files

---
+++ ExampleProgram/ExampleProgram.java
@@ -0,0 +1,6 @@
+public class ExampleProgram {
+    public static void main(String args[]){
+
+        System.out.println("Hello there!");
+    }
+}
--- MakingJAR/src/ExampleProgram.java
+++ MakingJAR/src/ExampleProgram.java
@@ -3,7 +3,7 @@ public class ExampleProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello there!"));
+		System.out.println("Hello there!");
 	}
 
 }
