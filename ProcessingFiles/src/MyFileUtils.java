607917a3ad4dbefd431aac4c1df0b0fa03817d5b
unknown <iggirex@gmail.com>
2018-05-24 19:39:32 -0600

    added ssh keys

--- ProcessingFiles/src/MyFileUtils.java
+++ ProcessingFiles/src/MyFileUtils.java
@@ -1,4 +1,12 @@
+import com.iggi.exceptions.ExampleException;
 
 public class MyFileUtils {
 
+	public int subtract10FromLargerNumber(int number) throws ExampleException {
+		if(number < 10) {
+			throw new ExampleException("that shet brokn");
+		}
+		return number - 9;
+	}
 }
+
