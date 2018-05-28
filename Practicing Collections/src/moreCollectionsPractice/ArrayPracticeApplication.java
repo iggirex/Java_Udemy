3427a630dbe0589f62f5fe46b089d52f32b2fe17
unknown <iggirex@gmail.com>
2018-05-28 12:31:11 -0600

    making arrays from objects

--- Practicing Collections/src/moreCollectionsPractice/ArrayPracticeApplication.java
+++ Practicing Collections/src/moreCollectionsPractice/ArrayPracticeApplication.java
@@ -9,8 +9,14 @@ public class ArrayPracticeApplication {
 		
 		
 		String[] myNewArr = ArrayUtil.makeStringArray("hey", "may", "array");
-		
 		System.out.println(Arrays.toString(myNewArr));
+		
+		
+		PureArrayFromObj myObj = new PureArrayFromObj();
+		
+		String[] madeFromObj = myObj.makeStringArray("dis", "array", "from obj");
+		
+		System.out.println(Arrays.toString(madeFromObj));
 		
 
 	}
--- Practicing Collections/src/moreCollectionsPractice/PureArrayFromObj.java
+++ Practicing Collections/src/moreCollectionsPractice/PureArrayFromObj.java
@@ -2,20 +2,19 @@ package moreCollectionsPractice;
 
 public class PureArrayFromObj {
 	
-	private static final String str1 = null;
-	private Object makeStringArray;
-
 	public PureArrayFromObj() {
-		this.makeStringArray = makeStringArray;
+		
 	}
 	
 	public String[] makeStringArray(String str1, String str2, String str3) {
 		String[] result = new String[3];
 		result[0] = str1;
-		result[1] = str1;
-		result[2] = str1;
+		result[1] = str2;
+		result[2] = str3;
 		
 		return result;
 //		
 	}
+	
+
 }
